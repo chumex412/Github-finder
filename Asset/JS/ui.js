d6e0c079895f64ad4add991e2ad9663459fd57bf
chumex412 <64347090+chumex412@users.noreply.github.com>
2021-02-28 14:31:52 +0100

    Added error alert and displayed output

--- Asset/JS/ui.js
+++ Asset/JS/ui.js
@@ -18,8 +18,8 @@ class UI {
               <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
               <span class="badge badge-success">Followers: ${user.followers}</span>
               <span class="badge badge-info">Following: ${user.following}</span>
-
-              <ul class="unordered-list list-unstyled parag mt-2">
+              <br/><br/>
+              <ul class="unordered-list list-unstyled parag">
                 <li class="list-item">Company: ${user.company}</li>
                 <li class="list-item">Website/blog: ${user.blog}</li>
                 <li class="list-item">Location: ${user.location}</li>
@@ -29,8 +29,70 @@ class UI {
           </div>
         </div>
       </div>
+      <h2 class="h3">Latest Repos</h2>
+      <div id="repos"></div>
     `;
   }
 
+  // Display Repos
+  showRepos(repos) {
+    let output = '';
+
+    repos.forEach(repo => {
+      output += `
+        <br/>
+        <div class="card mb-2">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-3">
+                <a href="${repo.html_url}" class="parag link" target="_blank">${repo.name}</a>
+              </div>
+
+              <div class="col-3">
+                <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+                <span class="badge badge-success">Watchers: ${repo.watchers_count}</span>
+                <span class="badge badge-warning">Forks: ${repo.forks_count}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br/>
+      `;
+    });
+    // Output the Repos to the DOM
+    document.querySelector('#repos').innerHTML = output;
+  }
+
+  // Show alert
+  showAlert(message, className) {
+    // Clear alert
+    this.clearAlert();
+    // UI element
+    const container = document.querySelector('#searchContainer');
+    const card = document.querySelector('.search');
+    // Create div
+    const div = document.createElement('div');
+    div.className = className;
+    div.append(document.createTextNode(message));
+    container.insertBefore(div, card);
+
+    // Clear alert after 3 seconds
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+
+  // Clear alert message
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+    if(currentAlert) {
+      currentAlert.remove();
+    }
+  } 
+
+  // Clear profile
+  clearProfile() {
+    this.profile.innerHTML = "";
+  }
 
 }
